f2610f721b7a3bcb35031ca88c10b8a3270fafab
Nayef Al Baloushi <Nayef@Nayefs-MacBook-Pro.local>
2014-03-05 16:15:47 +0400

    added a test class

---
+++ Git Test/NBTest.h
@@ -0,0 +1,13 @@
+//
+//  NBTest.h
+//  Git Test
+//
+//  Created by Nayef Al Baloushi on 3/5/14.
+//  Copyright (c) 2014 Nayef Al Baloushi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NBTest : NSObject
+
+@end
---
+++ Git Test/NBTest.m
@@ -0,0 +1,13 @@
+//
+//  NBTest.m
+//  Git Test
+//
+//  Created by Nayef Al Baloushi on 3/5/14.
+//  Copyright (c) 2014 Nayef Al Baloushi. All rights reserved.
+//
+
+#import "NBTest.h"
+
+@implementation NBTest
+
+@end
